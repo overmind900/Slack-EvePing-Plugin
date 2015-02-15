1d493fe44d823e5c1b5489cc39c1074bda75d8c4
Darren Maginnis <overmind.900@gmail.com>
2015-02-15 18:48:29 -0500

    using wrapped ef

--- SlackEvePingPlugin/DataLayer.cs
+++ SlackEvePingPlugin/DataLayer.cs
@@ -14,11 +14,13 @@ namespace SlackEvePingPlugin {
 			keyID = null;
 			vCode = null;
 			keyType = KeyType.Corporation;
-			SlackEvePingEntities ef = new SlackEvePingEntities();
-			UserMapping user = ef.UserMappings.FirstOrDefault(x => x.UserID == user_id.Trim());
-			if( user != null ) {
-				keyID = user.KeyID;
-				vCode = user.vCode;
+			UserMapping user;
+			using (SlackEvePingEntities ef = new SlackEvePingEntities()){
+				user = ef.UserMappings.FirstOrDefault(x => x.UserID == user_id.Trim());
+				if( user != null ) {
+					keyID = user.KeyID;
+					vCode = user.vCode;
+				}
 			}
 			return user != null;
 		}
@@ -27,39 +29,42 @@ namespace SlackEvePingPlugin {
 			if(string.IsNullOrWhiteSpace(user_id)) throw new ArgumentException("user_id cannot be empty");
 			if( string.IsNullOrWhiteSpace(keyID) ) throw new ArgumentException("KeyID cannot be empty");
 			if( string.IsNullOrWhiteSpace(vCode) ) throw new ArgumentException("vCode cannot be empty");
-			SlackEvePingEntities ef = new SlackEvePingEntities();
-			UserMapping user = ef.UserMappings.FirstOrDefault(x => x.UserID == user_id.Trim());
-			if( user == null ) {
-				user = new UserMapping();
-				user.UserID = user_id.Trim();
-				ef.UserMappings.AddObject(user);
+			using (SlackEvePingEntities ef = new SlackEvePingEntities()){
+				UserMapping user = ef.UserMappings.FirstOrDefault(x => x.UserID == user_id.Trim());
+				if( user == null ) {
+					user = new UserMapping();
+					user.UserID = user_id.Trim();
+					ef.UserMappings.AddObject(user);
+				}
+				user.KeyID = keyID.Trim();
+				user.vCode = vCode.Trim();
+				user.KeyType = KeyType.ToString();
+				ef.SaveChanges();
 			}
-			user.KeyID = keyID.Trim();
-			user.vCode = vCode.Trim();
-			user.KeyType = KeyType.ToString();
-			ef.SaveChanges();
 			return true;
 		}
 
 		internal static bool Remove(string user_id) {
 			if( string.IsNullOrWhiteSpace(user_id) ) throw new ArgumentException("user_id cannot be empty");
 			bool success = false;
-			SlackEvePingEntities ef = new SlackEvePingEntities();
-			UserMapping user = ef.UserMappings.FirstOrDefault(x => x.UserID == user_id.Trim());
-			if( user != null ) {
-				ef.UserMappings.DeleteObject(user);
-				ef.SaveChanges();
-				success = true;
+			using(SlackEvePingEntities ef = new SlackEvePingEntities()){
+				UserMapping user = ef.UserMappings.FirstOrDefault(x => x.UserID == user_id.Trim());
+				if( user != null ) {
+					ef.UserMappings.DeleteObject(user);
+					ef.SaveChanges();
+					success = true;
+				}
 			}
 			return success;
 		}
 
 		internal static void Log(string message) {
-			SlackEvePingEntities ef = new SlackEvePingEntities();
-			SlackEvePingPlugin.Log logMessage = new SlackEvePingPlugin.Log();
-			logMessage.Message = message;
-			logMessage.DateTime = DateTime.UtcNow;
-			ef.Logs.AddObject(logMessage);
+			using(SlackEvePingEntities ef = new SlackEvePingEntities()){
+				SlackEvePingPlugin.Log logMessage = new SlackEvePingPlugin.Log();
+				logMessage.Message = message;
+				logMessage.DateTime = DateTime.UtcNow;
+				ef.Logs.AddObject(logMessage);
+			}
 		}
 
 
